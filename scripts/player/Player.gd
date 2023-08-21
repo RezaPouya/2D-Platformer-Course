5ef71140dba2319c88936090b8335edabc6e7cd6
RezaPouya <rpouya@hotmail.com>
2023-08-21 23:33:15 +0330

    auto tile and player movement script

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -1,12 +1,20 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var gravity = 300
-var playerVelocity = Vector2.ZERO # Vector2(0,0)
-var playerMaxHorizentalSpeed = 110
-var playerMaxJumpSpeed = 150
+var gravity = 1000
+var velocity = Vector2.ZERO # Vector2(0,0)
+var maxHorizontalSpeed = 140
+var jumpSpeed = 340
+var horizontalAcceleration = 2000 
+var jumpTerminationMultiplier =  4
+var maxDashSpeed = 500
+var minDashSpeed = 200
+var hasDoubleJump = false
+var hasDash = false
+#var currentState = State.NORMAL
+var isStateNew = true
+var isDying = false
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,23 +23,34 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var moveVector = setInitialmoveVector()
 	
-	playerVelocity.y += gravity * delta
-	
-	playerVelocity.x = move().x * playerMaxHorizentalSpeed
-	
-	if (jump().y < 0 && is_on_floor()):
-		playerVelocity.y = jump().y * playerMaxJumpSpeed
+	velocity.x += moveVector.x * horizontalAcceleration * delta
+	if (moveVector.x == 0):
+		velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
 		
-	playerVelocity = move_and_slide(playerVelocity , Vector2.UP)
+	velocity.x += moveVector.x * horizontalAcceleration * delta
 	
-func move():
+	# if we dont have any input in x , we gonna decelarate
+	if(velocity.x == 0):
+		# velocity.x = lerp(velocity.x , 0 , 0.1)
+		velocity.x = lerp( 0 , velocity.x , pow(2 , -50 * delta)) # frame rate independent lerp
+	
+	# this is gonna prevent us to go above speed limit 
+	velocity.x = clamp(velocity.x , -maxHorizontalSpeed , maxHorizontalSpeed)
+	
+	if (moveVector.y < 0 && is_on_floor()):
+		velocity.y = moveVector.y * jumpSpeed
+		
+	if (velocity.y < 0 && !Input.is_action_pressed("jump")):
+		velocity.y += gravity * jumpTerminationMultiplier * delta
+	else:
+		velocity.y += gravity * delta
+		
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+func setInitialmoveVector():
 	var moveVector = Vector2.ZERO
 	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	return moveVector
-	
-func jump():
-	var moveVector = Vector2.ZERO
 	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
 	return moveVector
-
